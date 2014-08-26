f974e10de7f6ef409cd268c2b35f3fa9a1662297
Frank Lemanschik <frank@dspeed.eu>
2014-08-26 22:28:04 +0200

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 pushd /tmp
 
-git clone git://github.com/sstephenson/rbenv.git /home/ubuntu/.rbenv              && \
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv              && \
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 export HOME=/home/openproject
