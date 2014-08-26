888aceaa0dcd66edf1feb7612cff36fa7c981370
Frank Lemanschik <frank@dspeed.eu>
2014-08-26 22:21:58 +0200

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -4,7 +4,8 @@ pushd /tmp
 git clone git://github.com/sstephenson/rbenv.git /home/ubuntu/.rbenv              && \
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-export PATH=$PATH:/home/ubuntu/.rbenv/bin
+export HOME=/home/openproject
+export PATH=$PATH:/home/openproject/.rbenv/bin
 export RBENV_VERSION=2.1.0
 eval "$(rbenv init -)"
 CC=gcc rbenv install 2.1.0
